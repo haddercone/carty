cd1b838ce050ee34364658a67b7850b10df96318
Robin Singh <44robin.rs@gmail.com>
2023-06-07 22:43:49 +0530

    no product result handling added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -41,38 +41,41 @@ const Products = ({ products, setProducts }) => {
         </button>
       </div>
       <div className="flex  gap-2 md:gap-10 bg-white flex-wrap items-center justify-center md:p-3">
-        {products &&
-          products.map((product) => {
-            const actualPrice = Math.ceil(
-              (product?.price * 100) / (100 - product?.discountPercentage)
-            );
-            return (
-              <div
-                key={product?.id}
-                className="w-44 h-auto  md:w-72 xl:w-80 border-2 p-3 hover:border-2 "
-              >
-                <div className="overflow-hidden">
-                  <img
-                    src={product?.thumbnail}
-                    className="object-cover w-36 h-36 md:w-72 xl:w-80 md:h-80 "
-                  />
-                </div>
-                <p className="my-3 text-xl">{product.title}</p>
-                <p className="pb-3 hidden md:block">
-                  <span className="px-3 py-2 rounded-2xl text-sm bg-orange-200 text-orange-800">
-                    {product?.category}
-                  </span>
-                </p>
-                <div className="flex gap-2 md:gap-4 md:flex-row flex-col">
-                  <p className="font-bold text-xl">${product?.price}</p>
-                  <p className="text-gray-500  line-through">${actualPrice}</p>
-                  <p className=" font-bold text-md text-green-700">
-                    {product.discountPercentage}% off
+        {products.length === 0 
+          ? <p className="text-6xl font-light h-full">No products found</p>
+          : products.map((product) => {
+              const actualPrice = Math.ceil(
+                (product?.price * 100) / (100 - product?.discountPercentage)
+              );
+              return (
+                <div
+                  key={product?.id}
+                  className="w-44   md:w-72 xl:w-80 border-2 p-3 hover:border-2 "
+                >
+                  <div className="overflow-hidden">
+                    <img
+                      src={product?.thumbnail}
+                      className="object-cover w-36 h-36 md:w-72 xl:w-80 md:h-80 "
+                    />
+                  </div>
+                  <p className="my-3 text-xl">{product.title}</p>
+                  <p className="pb-3 hidden md:block">
+                    <span className="px-3 py-2 rounded-2xl text-sm bg-orange-200 text-orange-800">
+                      {product?.category}
+                    </span>
                   </p>
+                  <div className="flex gap-2 md:gap-4 md:flex-row flex-col">
+                    <p className="font-bold text-xl">${product?.price}</p>
+                    <p className="text-gray-500  line-through">
+                      ${actualPrice}
+                    </p>
+                    <p className=" font-bold text-md text-green-700">
+                      {product.discountPercentage}% off
+                    </p>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
       </div>
     </>
   );
